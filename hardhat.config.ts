0ec9f4a8ffe36b1a92698dfcfc508e998df67bcd
Ian Macalinao <github@igm.pub>
2021-04-18 02:11:08 -0500

    Clean up code for package export

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -6,7 +6,6 @@ import "@ubeswap/hardhat-celo";
 import { fornoURLs, ICeloNetwork } from "@ubeswap/hardhat-celo";
 import "dotenv/config";
 import { parseEther } from "ethers/lib/utils";
-import { mkdir, writeFile } from "fs/promises";
 import "hardhat-abi-exporter";
 import "hardhat-gas-reporter";
 import { removeConsoleLog } from "hardhat-preprocessor";
@@ -19,29 +18,6 @@ task("deploy", "Deploys a step", (async (...args) =>
   (await import("./tasks/deploy")).deploy(...args)) as ActionType<{
   step: string;
 }>).addParam("step", "The step to deploy");
-
-task<{ name: string }>(
-  "metadata:write",
-  "Writes the metadata of a contract to disk",
-  async ({ name }, hre) => {
-    const artifact = await hre.artifacts.readArtifact(name);
-    const fqn = `${artifact.sourceName}:${artifact.contractName}`;
-    const info = await hre.artifacts.getBuildInfo(fqn);
-    const metadataStr = (info?.output.contracts[artifact.sourceName]?.[
-      artifact.contractName
-    ] as {
-      metadata?: string;
-    })?.metadata;
-    if (!metadataStr) {
-      throw new Error("metadata not found");
-    }
-    const contractDir = `${__dirname}/build/metadata/${artifact.contractName}`;
-    const outfile = `${contractDir}/metadata.json`;
-    await mkdir(contractDir, { recursive: true });
-    await writeFile(outfile, metadataStr);
-    console.log("Wrote to " + outfile);
-  }
-).addParam("name", "The name of the contract");
 
 const accounts: HDAccountsUserConfig = {
   mnemonic:
@@ -92,7 +68,6 @@ export default {
     },
   },
   paths: {
-    deploy: "scripts/deploy",
     sources: "./contracts",
     tests: "./test",
     cache: "./build/cache",
